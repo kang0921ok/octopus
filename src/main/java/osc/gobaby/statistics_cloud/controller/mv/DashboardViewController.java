b6aed03fb01b56b4236e3225f891025a977c2ca0
한상훈 <tkdgns688@gmail.com>
2018-10-01 03:03:20 +0900

    add dashboard menu

--- src/main/java/osc/gobaby/statistics_cloud/controller/mv/DashboardViewController.java
+++ src/main/java/osc/gobaby/statistics_cloud/controller/mv/DashboardViewController.java
@@ -1,13 +1,22 @@
 package osc.gobaby.statistics_cloud.controller.mv;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DashboardViewController {
 	
 	@RequestMapping("/dashboard")
-	public String dashboard() {
+	public String dashboardView(Model model) {
+		model.addAttribute("menu", "query_schema");
+		return "dashboard";
+	}
+	
+	@RequestMapping("/dashboard/{menu}")
+	public String dashboard(@PathVariable String menu, Model model) {
+		model.addAttribute("menu", menu);
 		return "dashboard";
 	}
 	
