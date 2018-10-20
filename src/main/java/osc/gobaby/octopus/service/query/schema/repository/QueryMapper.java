d6e58a865c31e98e859eff4c6108d10cea121b26
kang0921ok <kang0921ok@naver.com>
2018-10-20 20:28:20 +0900

    * security start

---
+++ src/main/java/osc/gobaby/octopus/service/query/schema/repository/QueryMapper.java
@@ -0,0 +1,18 @@
+package osc.gobaby.octopus.query.schema.repository;
+
+
+import osc.gobaby.octopus.query.schema.entity.Query;
+import osc.gobaby.octopus.support.BaseMapper;
+
+import java.util.List;
+
+/**
+ * Created by ShinHyun.Kang on 2018. 9. 9..
+ */
+public interface QueryMapper extends BaseMapper {
+    public Query selectOne(String userId);
+    public List<Query> selectList();
+    public List<Query> selectListForUserId(String userId);
+    public int insert(Query query);
+    public int update(Query query);
+}
